b8884940c080a1f084b3952b5c14aaf6c2bcee62
katatonia <pmilekhina@gmail.com>
2025-03-03 16:11:24 +0400

    feature: changes footer

--- source/js/modules/accordion.js
+++ source/js/modules/accordion.js
@@ -2,7 +2,16 @@ export const accordion = () => {
 	const menuTitle = document.querySelector(".menu__title");
 	const menuList = document.querySelector(".menu__list");
 
+	const updateBodyScrollLock = () => {
+		if (nav.classList.contains("nav_is-open") || orders.classList.contains("orders_is-open")) {
+			document.body.classList.add("scroll-lock");
+		} else {
+			document.body.classList.remove("scroll-lock");
+		}
+	};
+
 	menuTitle.addEventListener("click", function () {
 		menuList.classList.toggle('menu__list_is-open');
+		updateBodyScrollLock();
 	});
 };
