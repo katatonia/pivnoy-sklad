c16fdcfcf459b0d7092ed3868e57a0228762f4d0
katatonia <pmilekhina@gmail.com>
2025-03-11 15:43:03 +0400

    feature: glass-panel

--- source/js/modules/favorite-icon.js
+++ source/js/modules/favorite-icon.js
@@ -2,7 +2,9 @@ export const toggleFavoriite = () => {
 	const favoriteButton = document.querySelector('.product__favorite');
 	const favoriteIcon = document.querySelector('.product__favorite-icon');
 
-	favoriteButton.addEventListener('click', () => {
-		favoriteIcon.classList.toggle('product__favorite-icon_is-active');
-	})
-}
+	if (favoriteButton && favoriteIcon) {
+		favoriteButton.addEventListener('click', () => {
+			favoriteIcon.classList.toggle('product__favorite-icon_is-active');
+		})
+	}
+};
